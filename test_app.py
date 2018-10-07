db92654a9f436321c5b17953a59a33e65dac3bcc
Isaac Imholt <isaacimholt@gmail.com>
2018-10-07 20:06:38 +0200

    first banal test for app

--- test_app.py
+++ test_app.py
@@ -1,2 +1,18 @@
-def test_true():
-    assert False
+import pytest
+
+from app import app
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    yield client
+
+
+def test_empty_db(client):
+    """Start with a blank database."""
+
+    rv = client.get('/')
+    assert b'aud' in rv.data
