c86a7d6231a0cbcb162cca76db0601edb9c7285e
Isaac Imholt <isaacimholt@gmail.com>
2018-10-09 15:47:33 +0200

    finished tests

--- test_app.py
+++ test_app.py
@@ -1,3 +1,4 @@
+import json
 import pytest
 
 from app import app
@@ -11,8 +12,39 @@ def client():
     yield client
 
 
-def test_empty_db(client):
-    """Start with a blank database."""
+# list of test cases that should pass
+test_cases = [
+    # ({request},
+    #  {expected response})
+    ({
+         # reflection
+         'reference_date': '2018-09-10',
+         'amount':         1.2,
+         'src_currency':   'EUR',
+         'dest_currency':  'EUR'
+     },
+     {
+         'amount':   1.2,
+         'currency': 'EUR'
+     }),
 
-    rv = client.get('/')
-    assert b'aud' in rv.data
+    ({
+         # conversion
+         'reference_date': '2018-09-04',
+         'amount':         1.5,
+         'src_currency':   'SGD',
+         'dest_currency':  'ISK'
+     },
+     {
+         'amount':   118.8800201118723,
+         'currency': 'ISK'
+     }),
+]
+
+
+@pytest.mark.parametrize("test_data, expected_data", test_cases)
+def test_rate_converter(test_data, expected_data, client):
+    """Test predefined currency conversions."""
+
+    rv = client.get('/convert', data=test_data)
+    assert json.loads(rv.data) == expected_data
